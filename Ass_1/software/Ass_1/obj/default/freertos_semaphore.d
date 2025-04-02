df99b986f4649bb7caceea9e33d090cb95ba58c1
Suppise <tcas362@aucklanduni.ac.nz>
2025-04-02 15:52:36 +1300

    timers and stability flag base code

--- Ass_1/software/Ass_1/obj/default/freertos_semaphore.d
+++ Ass_1/software/Ass_1/obj/default/freertos_semaphore.d
@@ -4,7 +4,10 @@ obj/default/freertos_semaphore.o: freertos_semaphore.c \
  ../Ass_1_bsp//HAL/inc/alt_types.h \
  ../Ass_1_bsp//HAL/inc/priv/alt_legacy_irq.h ../Ass_1_bsp//HAL/inc/io.h \
  ../Ass_1_bsp//HAL/inc/alt_types.h \
- ../Ass_1_bsp//drivers/inc/altera_avalon_pio_regs.h
+ ../Ass_1_bsp//drivers/inc/altera_avalon_pio_regs.h freertos/FreeRTOS.h \
+ freertos/FreeRTOSConfig.h freertos/projdefs.h freertos/portable.h \
+ freertos/deprecated_definitions.h freertos/portmacro.h \
+ freertos/mpu_wrappers.h freertos/task.h freertos/list.h freertos/queue.h
 
 ../Ass_1_bsp/system.h:
 
@@ -23,3 +26,23 @@ obj/default/freertos_semaphore.o: freertos_semaphore.c \
 ../Ass_1_bsp//HAL/inc/alt_types.h:
 
 ../Ass_1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+freertos/FreeRTOS.h:
+
+freertos/FreeRTOSConfig.h:
+
+freertos/projdefs.h:
+
+freertos/portable.h:
+
+freertos/deprecated_definitions.h:
+
+freertos/portmacro.h:
+
+freertos/mpu_wrappers.h:
+
+freertos/task.h:
+
+freertos/list.h:
+
+freertos/queue.h:
---
+++ Ass_1/software/Ass_1/obj/default/main.d
@@ -0,0 +1,52 @@
+obj/default/main.o: main.c ../Ass_1_bsp//HAL/inc/sys/alt_irq.h \
+ ../Ass_1_bsp//HAL/inc/nios2.h ../Ass_1_bsp//HAL/inc/alt_types.h \
+ ../Ass_1_bsp/system.h ../Ass_1_bsp/linker.h \
+ ../Ass_1_bsp//HAL/inc/priv/alt_legacy_irq.h ../Ass_1_bsp//HAL/inc/io.h \
+ ../Ass_1_bsp//HAL/inc/alt_types.h \
+ ../Ass_1_bsp//drivers/inc/altera_avalon_pio_regs.h freertos/FreeRTOS.h \
+ freertos/FreeRTOSConfig.h freertos/projdefs.h freertos/portable.h \
+ freertos/deprecated_definitions.h freertos/portmacro.h \
+ freertos/mpu_wrappers.h freertos/task.h freertos/list.h freertos/queue.h \
+ FreeRTOS/timers.h FreeRTOS/task.h
+
+../Ass_1_bsp//HAL/inc/sys/alt_irq.h:
+
+../Ass_1_bsp//HAL/inc/nios2.h:
+
+../Ass_1_bsp//HAL/inc/alt_types.h:
+
+../Ass_1_bsp/system.h:
+
+../Ass_1_bsp/linker.h:
+
+../Ass_1_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../Ass_1_bsp//HAL/inc/io.h:
+
+../Ass_1_bsp//HAL/inc/alt_types.h:
+
+../Ass_1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+freertos/FreeRTOS.h:
+
+freertos/FreeRTOSConfig.h:
+
+freertos/projdefs.h:
+
+freertos/portable.h:
+
+freertos/deprecated_definitions.h:
+
+freertos/portmacro.h:
+
+freertos/mpu_wrappers.h:
+
+freertos/task.h:
+
+freertos/list.h:
+
+freertos/queue.h:
+
+FreeRTOS/timers.h:
+
+FreeRTOS/task.h:
