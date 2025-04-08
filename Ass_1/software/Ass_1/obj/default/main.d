266a9bdc25c62cf62fc46255b39da835dd83d2cf
Suppise <tcas362@aucklanduni.ac.nz>
2025-04-08 12:09:02 +1200

    vga test 1

--- Ass_1/software/Ass_1/obj/default/main.d
+++ Ass_1/software/Ass_1/obj/default/main.d
@@ -3,11 +3,16 @@ obj/default/main.o: main.c ../Ass_1_bsp//HAL/inc/sys/alt_irq.h \
  ../Ass_1_bsp/system.h ../Ass_1_bsp/linker.h \
  ../Ass_1_bsp//HAL/inc/priv/alt_legacy_irq.h ../Ass_1_bsp//HAL/inc/io.h \
  ../Ass_1_bsp//HAL/inc/alt_types.h \
- ../Ass_1_bsp//drivers/inc/altera_avalon_pio_regs.h freertos/FreeRTOS.h \
- freertos/FreeRTOSConfig.h freertos/projdefs.h freertos/portable.h \
- freertos/deprecated_definitions.h freertos/portmacro.h \
- freertos/mpu_wrappers.h freertos/task.h freertos/list.h freertos/queue.h \
- FreeRTOS/timers.h FreeRTOS/task.h
+ ../Ass_1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Ass_1_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h \
+ ../Ass_1_bsp//HAL/inc/sys/alt_dev.h \
+ ../Ass_1_bsp//HAL/inc/sys/alt_llist.h \
+ ../Ass_1_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../Ass_1_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+ freertos/FreeRTOS.h freertos/FreeRTOSConfig.h freertos/projdefs.h \
+ freertos/portable.h freertos/deprecated_definitions.h \
+ freertos/portmacro.h freertos/mpu_wrappers.h freertos/task.h \
+ freertos/list.h freertos/queue.h FreeRTOS/timers.h FreeRTOS/task.h
 
 ../Ass_1_bsp//HAL/inc/sys/alt_irq.h:
 
@@ -26,6 +31,16 @@ obj/default/main.o: main.c ../Ass_1_bsp//HAL/inc/sys/alt_irq.h \
 ../Ass_1_bsp//HAL/inc/alt_types.h:
 
 ../Ass_1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Ass_1_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h:
+
+../Ass_1_bsp//HAL/inc/sys/alt_dev.h:
+
+../Ass_1_bsp//HAL/inc/sys/alt_llist.h:
+
+../Ass_1_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../Ass_1_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
 
 freertos/FreeRTOS.h:
 
