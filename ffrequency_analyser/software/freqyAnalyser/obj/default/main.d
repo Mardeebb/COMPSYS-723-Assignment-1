2702f0e0d9461f701d561ca2daed89021ec86764
Mardeebb <121736600+Mardeebb@users.noreply.github.com>
2025-04-08 14:27:14 +1200

    load managing state working

--- ffrequency_analyser/software/freqyAnalyser/obj/default/main.d
+++ ffrequency_analyser/software/freqyAnalyser/obj/default/main.d
@@ -1,30 +1,50 @@
-obj/default/main.o: main.c ../freqyAnalyser_bsp/system.h \
- ../freqyAnalyser_bsp/linker.h \
+obj/default/main.o: main.c ../freqyAnalyser_bsp//HAL/inc/io.h \
+ ../freqyAnalyser_bsp//HAL/inc/alt_types.h \
+ ../freqyAnalyser_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h \
+ ../freqyAnalyser_bsp//HAL/inc/alt_types.h \
+ ../freqyAnalyser_bsp//HAL/inc/sys/alt_dev.h \
+ ../freqyAnalyser_bsp/system.h ../freqyAnalyser_bsp/linker.h \
+ ../freqyAnalyser_bsp//HAL/inc/sys/alt_llist.h \
+ ../freqyAnalyser_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../freqyAnalyser_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
  ../freqyAnalyser_bsp//HAL/inc/sys/alt_alarm.h \
  ../freqyAnalyser_bsp//HAL/inc/sys/alt_llist.h \
- ../freqyAnalyser_bsp//HAL/inc/alt_types.h \
  ../freqyAnalyser_bsp//HAL/inc/priv/alt_alarm.h \
  ../freqyAnalyser_bsp//HAL/inc/sys/alt_irq.h \
  ../freqyAnalyser_bsp//HAL/inc/nios2.h \
  ../freqyAnalyser_bsp//HAL/inc/priv/alt_legacy_irq.h \
  ../freqyAnalyser_bsp//drivers/inc/altera_avalon_pio_regs.h \
- ../freqyAnalyser_bsp//HAL/inc/io.h \
- ../freqyAnalyser_bsp//HAL/inc/alt_types.h \
  ../freqyAnalyser_bsp//drivers/inc/altera_avalon_uart_regs.h \
  FreeRTOS/FreeRTOS.h FreeRTOS/FreeRTOSConfig.h FreeRTOS/projdefs.h \
  FreeRTOS/portable.h FreeRTOS/deprecated_definitions.h \
  FreeRTOS/portmacro.h FreeRTOS/mpu_wrappers.h FreeRTOS/task.h \
- FreeRTOS/list.h FreeRTOS/queue.h FreeRTOS/timers.h FreeRTOS/task.h
+ FreeRTOS/list.h FreeRTOS/queue.h FreeRTOS/timers.h FreeRTOS/task.h \
+ FreeRTOS/portable.h FreeRTOS/portmacro.h FreeRTOS/semphr.h \
+ FreeRTOS/queue.h
+
+../freqyAnalyser_bsp//HAL/inc/io.h:
+
+../freqyAnalyser_bsp//HAL/inc/alt_types.h:
+
+../freqyAnalyser_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h:
+
+../freqyAnalyser_bsp//HAL/inc/alt_types.h:
+
+../freqyAnalyser_bsp//HAL/inc/sys/alt_dev.h:
 
 ../freqyAnalyser_bsp/system.h:
 
 ../freqyAnalyser_bsp/linker.h:
 
+../freqyAnalyser_bsp//HAL/inc/sys/alt_llist.h:
+
+../freqyAnalyser_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../freqyAnalyser_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
 ../freqyAnalyser_bsp//HAL/inc/sys/alt_alarm.h:
 
 ../freqyAnalyser_bsp//HAL/inc/sys/alt_llist.h:
-
-../freqyAnalyser_bsp//HAL/inc/alt_types.h:
 
 ../freqyAnalyser_bsp//HAL/inc/priv/alt_alarm.h:
 
@@ -35,10 +55,6 @@ obj/default/main.o: main.c ../freqyAnalyser_bsp/system.h \
 ../freqyAnalyser_bsp//HAL/inc/priv/alt_legacy_irq.h:
 
 ../freqyAnalyser_bsp//drivers/inc/altera_avalon_pio_regs.h:
-
-../freqyAnalyser_bsp//HAL/inc/io.h:
-
-../freqyAnalyser_bsp//HAL/inc/alt_types.h:
 
 ../freqyAnalyser_bsp//drivers/inc/altera_avalon_uart_regs.h:
 
@@ -65,3 +81,11 @@ FreeRTOS/queue.h:
 FreeRTOS/timers.h:
 
 FreeRTOS/task.h:
+
+FreeRTOS/portable.h:
+
+FreeRTOS/portmacro.h:
+
+FreeRTOS/semphr.h:
+
+FreeRTOS/queue.h:
